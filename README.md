f2e2bfa3fa5b595f9c5aa11838a4bba1b2b6c59f
Wewill <contact@wilhemarnoldy.fr>
2023-02-20 10:09:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue2-vue-stripe-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue2-vue-cli-9ha1fi)
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Stripe />
+</template>
+
+<script>
+import Stripe from './components/Stripe.vue'
+
+export default {
+  components: {
+    Stripe,
+  },
+}
+</script>
